64decede5a8936c88e815b831d1148e526d89964
Bartek <bartek.polomski@gmail.com>
2018-09-08 13:44:51 +0200

    fsm_behaviour Behaviours of FSM with test

--- src/main/groovy/Fsm.groovy
+++ src/main/groovy/Fsm.groovy
@@ -5,12 +5,27 @@ import groovy.transform.Immutable
 class Fsm {
 
     Map transitions
+    String initState
+    String currentState
 
-    static def load(@DelegatesTo(value = FsmBuilder,strategy = Closure.DELEGATE_ONLY) Closure closure) {
+    static Fsm load(@DelegatesTo(value = FsmBuilder,strategy = Closure.DELEGATE_ONLY) Closure closure) {
         def fsmBuilder = new FsmBuilder()
         def fmsRecipe = closure.rehydrate(fsmBuilder, this, fsmBuilder)
         fmsRecipe.resolveStrategy = Closure.DELEGATE_ONLY
         fmsRecipe().build()
+    }
+
+
+    def state() {
+        currentState
+    }
+
+    def clear() {
+       ofCurrent(initState)
+    }
+
+    def fire(event) {
+        ofCurrent(from(event))
     }
 
     @Override
@@ -28,5 +43,17 @@ class Fsm {
 
     def propertyMissing(String name, def arg) {
         throw new FsmWordNotSupported(name)
+    }
+
+    private def ofCurrent(state){
+        new Fsm(transitions, initState, state ? state : currentState)
+    }
+
+    private def from(event) {
+        def state = {
+            transitions[event][0] == currentState ? transitions[event][1] : currentState
+        }
+
+        transitions[event] ? state() : currentState
     }
 }
